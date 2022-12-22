9f2dc3ee2ed527e00bb008e096e6367a6298f8cb
KarinaBertosh <cherrycake246@gmail.com>
2022-12-22 16:07:48 +0300

    feat: add footer

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import Header from "../Header/header";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 
 
 const MainPage = () => {
     return (
         <div className="main-page">
             <Header/>
+
+
+            <Footer/>
         </div>
     )
 }
