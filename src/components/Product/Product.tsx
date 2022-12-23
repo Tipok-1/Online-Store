e37593239aadab3d1f8d2a9e47d6a6b847703806
KarinaBertosh <cherrycake246@gmail.com>
2022-12-23 16:24:14 +0300

    feat: add product component

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import ButtonBootstrap from "../ButtonBootsrap/ButtonBootsrap";
 import '../Product/Product.css'
-import ProductHeader from "../ProductHeader/ProductHeader";
-import ProductImage from "../ProductImage/ProductImage";
 
 
-const Product = () => {
+const Product = ({title, category, brand, price, discount, rating, stock, thumbnail}) => {
+
+
     return (
         <div className='product'>
-            <ProductHeader/>
-            <ProductImage/>
+            <p className="product-header">{title}</p>
+            <div className="product-description">
+                <img className="product-img" src={thumbnail}></img>
+                <p>Category: {category}</p>
+                <p>Brand: {brand}</p>
+                <p>Price: {price}</p>
+                <p>Discount: {discount}</p>
+                <p>Rating: {rating}</p>
+                <p>Stock: {stock}</p>
+            </div>
+            <div className="product-btns">
+
+            <ButtonBootstrap>ADD TO CARD</ButtonBootstrap>
+            <ButtonBootstrap>DETAILS</ButtonBootstrap>
+            </div>
         </div>
     )
 }
--- src/components/ProductFieldCatalog/ProductFieldCatalog.tsx
+++ src/components/ProductFieldCatalog/ProductFieldCatalog.tsx
@@ -2,16 +2,34 @@ import React from "react";
 import Found from "../Found/Found";
 import Product from "../Product/Product";
 import '../ProductFieldCatalog/ProductFieldCatalog.css'
+import { data } from "../products";
 import SearchProduct from "../SearchProduct/SearchProduct";
 import SortOptions from "../SortOptions/SortOptions";
 
-// const arr = new Array(100).fill({ title: '1' })
-// console.log(arr);
 
 const ProductFieldCatalog = () => {
+
+    // const getProducts = data.products.map(product => product.title)
+    // console.log(getProducts);
+
+
+    //get data, mapping , props
     return (
         <div className='product-field-catalog'>
-            <Product />
+            {data.products.map(product => {
+                return (
+                    <Product 
+                        title={product.title} 
+                        category={product.category}
+                        brand={product.brand}
+                        price={product.price}
+                        discount={product.discountPercentage}
+                        rating={product.rating}
+                        stock={product.stock}
+                        thumbnail={product.thumbnail}
+                    />
+                )
+            })}
         </div>
     )
 }
