7343f1fda1172e2b96981d36d55b44deee42049a
KarinaBertosh <cherrycake246@gmail.com>
2022-12-30 10:11:06 +0300

    feat: add count product in basket

--- src/components/pages/BasketPage.tsx
+++ src/components/pages/BasketPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Button } from "react-bootstrap";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
@@ -9,14 +9,13 @@ import Product from "../Product/Product";
 
 const BasketPage = (): JSX.Element => {
     const storeCurrent = useContext(Store);
-    console.log(storeCurrent.products);
+    const [countProduct, setCountProduct] = useState(0);
     const result = []
+    let countPrice = 0;
     for (let i = 0; i < storeCurrent.products.length; i++) {
         const basketCurrent = data.products.filter((product) => product.id === storeCurrent.products[i])
         result.push(basketCurrent)
     }
-
-    console.log(result);
 
     return (
         <div className="basket-page">
@@ -30,36 +29,27 @@ const BasketPage = (): JSX.Element => {
                             {result.map(product => {
                                 return (
                                     <Product
-                                    title={product[0].title}
-                                    thumbnail={product[0].thumbnail}
-                                    category={product[0].category}
-                                    brand={product[0].brand}
-                                    price={product[0].price}
-                                    discountPercentage={product[0].discountPercentage}
-                                    stock={product[0].stock}
-                                    id={product[0].id}
-                                    rating={product[0].rating}
-                                />
+                                        title={product[0].title}
+                                        thumbnail={product[0].thumbnail}
+                                        category={product[0].category}
+                                        brand={product[0].brand}
+                                        price={product[0].price}
+                                        discountPercentage={product[0].discountPercentage}
+                                        stock={product[0].stock}
+                                        id={product[0].id}
+                                        rating={product[0].rating}
+                                        key={product[0].id}
+                                    />
                                 )
                             })}
-                            {/* {currentProduct
-                                && <Product
-                                    title={currentProduct.title}
-                                    thumbnail={currentProduct.thumbnail}
-                                    category={currentProduct.category}
-                                    brand={currentProduct.brand}
-                                    price={currentProduct.price}
-                                    discountPercentage={currentProduct.discountPercentage}
-                                    stock={currentProduct.stock}
-                                    id={currentProduct.id}
-                                    rating={currentProduct.rating}
-                                />} */}
+
                         </div>
                     </div>
                     <div className="basket-page__summary">
                         <div className="basket-page__title" >Summary</div>
-                        <div style={{ marginTop: 10 }} className="basket-page__summary__count-product" >Products: 1</div>
-                        <div style={{ margin: '10px 0' }} className="basket-page__summary__total" >Total: €0</div>
+                        <div style={{ marginTop: 10 }} className="basket-page__summary__count-product" >Products: {storeCurrent.products.length} </div>
+                        <div style={{ margin: '10px 0' }} className="basket-page__summary__total" >Total: € {countPrice}
+                        </div>
                         <Button>BUY NOW</Button>
                     </div>
                 </div>
